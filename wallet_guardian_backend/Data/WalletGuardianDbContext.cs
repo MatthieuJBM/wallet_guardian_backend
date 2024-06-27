4836018d71dad10f14848d78d303bbcb03541817
Mateusz Baryla <m.baryla@protonmail.ch>
2024-06-27 18:38:04 +0200

    feat: DbContext adjusted to the entities.

--- wallet_guardian_backend/Data/WalletGuardianDbContext.cs
+++ wallet_guardian_backend/Data/WalletGuardianDbContext.cs
@@ -7,4 +7,22 @@ public class WalletGuardianDbContext : DbContext
     public WalletGuardianDbContext(DbContextOptions options) : base(options)
     {
     }
+
+    public DbSet<Subcategory> Subcategories { get; set; }
+    public DbSet<Kategory> Kategories { get; set; }
+    public DbSet<Shop> Shops { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
+    public DbSet<MonthlyBudgetStatistic> MonthlyBudgetStatistics { get; set; }
+
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     base.OnModelCreating(modelBuilder);
+    //
+    //     modelBuilder.Entity<Kategories>().HasData(
+    //         new Kategory
+    //         {
+    //             
+    //         }
+    //         );
+    // }
 }
